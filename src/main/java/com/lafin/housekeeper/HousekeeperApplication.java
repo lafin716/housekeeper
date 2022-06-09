f2b1ad698949435efca4b8b61449bbc8d06242fd
lafinco <lafin716@naver.com>
2022-06-09 23:31:33 +0900

    Initial commit

---
+++ src/main/java/com/lafin/housekeeper/HousekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.lafin.housekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousekeeperApplication.class, args);
+	}
+
+}
