ee71fb8ff145f457ef35c6c188f0759cb697868e
lafinco <lafin716@naver.com>
2022-06-15 23:12:10 +0900

    feat: 비즈니스 오류 시 http status 변경

--- src/main/java/com/lafin/housekeeper/shared/util/ResponseUtil.java
+++ src/main/java/com/lafin/housekeeper/shared/util/ResponseUtil.java
@@ -1,12 +1,16 @@
 package com.lafin.housekeeper.shared.util;
 
 import com.lafin.housekeeper.shared.contract.presenter.viewmodel.ResponseModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseUtil {
 
     public static ResponseEntity<ResponseModel> json(ResponseModel response) {
+        if (response.isResult()) {
+            return ResponseEntity.ok(response);
+        }
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
